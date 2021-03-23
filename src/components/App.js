831e3d40bde55c76938d9ad6a10e5aeb16537f5f
Swati Singh <swsingh.in@gmail.com>
2021-03-23 19:11:09 +0100

    Defined App structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import Characters from './Characters';
+import { HOME_ROUTE, CHARACTER_ROUTE } from '../routes/routes';
 
-const App = () => (
-  <h1>Hello World</h1>
-);
+function Content() {
+  return (
+    <Switch>
+      <Route exact path={HOME_ROUTE} component={Home} />
+      <Route exact path={CHARACTER_ROUTE} component={Characters} />
+    </Switch>
+  );
+}
 
-export default App;
+export default Content;
