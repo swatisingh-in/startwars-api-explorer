8b12725e02f5d87c815bcbbd85899d615e506440
Swati Singh <swsingh.in@gmail.com>
2021-03-23 19:09:44 +0100

    Added URLs as constants

---
+++ src/components/Common/constants.js
@@ -0,0 +1,5 @@
+// get all the film resources
+
+// eslint-disable-next-line import/prefer-default-export
+export const STARWARS_MOVIES_URL = 'https://swapi.dev/api/films/';
+export const STARWARS_CHARACTERS_URL = 'https://swapi.dev/api/people/';
