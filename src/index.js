33d4d41b4e67ff85fd7c91dc5a105b3d6af72675
Swati Singh <swsingh.in@gmail.com>
2021-03-23 19:09:18 +0100

    enclose App in the BrowserRouter to support routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import App from './components/App';
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter history={history}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
